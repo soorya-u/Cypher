79daa8bc2ee4e69bfe6bf0ef6c4f0d595745ef65
Soorya U <sooryau7@gmail.com>
2024-04-16 18:33:12 +0530

    fix: Used Typesafe Route
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/components/custom/Navigator/index.tsx
+++ src/components/custom/Navigator/index.tsx
@@ -1,27 +1,26 @@
-import { Link, useRouterState } from "@tanstack/react-router";
+import { Link, useMatchRoute } from "@tanstack/react-router";
 
 export default function Navigator() {
-  const router = useRouterState();
-  const a = router.location.pathname;
+  const matchRoute = useMatchRoute();
+  const authRoute =
+    matchRoute({ to: "/signup" }) || matchRoute({ to: "/login" });
 
   return (
-    <div className="p-2 flex gap-2">
-      { true && a !== "/signup" && a !== "/login" && (
-        <>
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{" "}
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-          <Link to="/login" className="[&.active]:font-bold">
-            Login
-          </Link>
-          <Link to="/signup" className="[&.active]:font-bold">
-            Signup
-          </Link>
-        </>
-      )}
-    </div>
+    !authRoute && (
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+        <Link to="/login" className="[&.active]:font-bold">
+          Login
+        </Link>
+        <Link to="/signup" className="[&.active]:font-bold">
+          Signup
+        </Link>
+      </div>
+    )
   );
 }
