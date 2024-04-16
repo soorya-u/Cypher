8b758bc6f4843fdee04f8129c2ec8b6c31ab9f7c
Soorya U <sooryau7@gmail.com>
2024-04-16 15:15:34 +0530

    feat: Completed Routes
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,15 +1,7 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { createFileRoute } from "@tanstack/react-router";
+
+import Header from "@/components/custom/Header";
+import LoginCard from "@/components/custom/LoginCard";
 
 export const Route = createFileRoute("/login")({
   component: LogIn,
@@ -17,32 +9,9 @@ export const Route = createFileRoute("/login")({
 
 function LogIn() {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link to="/signup" className="underline">
-            Sign up
-          </Link>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Sign in</Button>
-      </CardFooter>
-    </Card>
+    <div className="w-full flex flex-col justify-center items-center mt-8 gap-y-6">
+      <Header />
+      <LoginCard />
+    </div>
   );
 }
