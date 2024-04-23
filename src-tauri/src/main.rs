b56e8aec489917f260925e4656c0fe4d91b3093a
Soorya U <sooryau7@gmail.com>
2024-04-23 10:41:14 +0530

    chore: Minor Rust Module Fixes
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod auth;
+mod cryptography;
+mod database;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-
-mod auth;
-mod cryptography;
-mod database;
 
 fn main() {
     tauri::Builder::default()
